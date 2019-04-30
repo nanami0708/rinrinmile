a08891714a8841c73ddb1da963273e13e65154db
nanami mizuno <the_seven_seas_773@icloud.com>
2019-05-01 01:10:33 +0900

    Initial Commit

---
+++ rinrinmile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rinrinmile
+//
+//  Created by Nanami Mizuno on 2019/05/01.
+//  Copyright © 2019 Nanami Mizuno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
